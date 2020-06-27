88bd8d01418e78d30968d3d352450ead41342278
lhcsm <67508824+lhcsm@users.noreply.github.com>
2020-06-27 22:21:29 +0800

    Update ad.js

--- ad.js
+++ ad.js
@@ -1,1 +1,1 @@
-[{"img":"https://sc01.alicdn.com/kf/U5727214714944ca2bea450e7fa72707bS.gif",url:"http://33711.hk"},{"img":"https://cbu01.alicdn.com/img/ibank/2020/646/468/13349864646_303742291.jpg",url:"http://33772a.com"}]
+[{"img":"https://sc01.alicdn.com/kf/U5727214714944ca2bea450e7fa72707bS.gif","url":"http://33711.hk"},{"img":"https://cbu01.alicdn.com/img/ibank/2020/646/468/13349864646_303742291.jpg","url":"http://33772a.com"}]
